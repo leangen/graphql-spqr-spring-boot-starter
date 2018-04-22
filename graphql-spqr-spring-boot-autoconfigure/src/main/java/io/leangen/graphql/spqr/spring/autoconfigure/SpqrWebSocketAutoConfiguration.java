89764eed5b6d15160ba37f0bfb92a676b311f8b8
Bojan Tomic <veggen@gmail.com>
2018-04-22 20:54:00 +0200

    Add WebSocket support only if spring-boot-starter-websocket is present

--- graphql-spqr-spring-boot-autoconfigure/src/main/java/io/leangen/graphql/spqr/spring/autoconfigure/SpqrWebSocketAutoConfiguration.java
+++ graphql-spqr-spring-boot-autoconfigure/src/main/java/io/leangen/graphql/spqr/spring/autoconfigure/SpqrWebSocketAutoConfiguration.java
@@ -3,6 +3,7 @@ package io.leangen.graphql.spqr.spring.autoconfigure;
 import graphql.schema.GraphQLSchema;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication;
 import org.springframework.context.annotation.Bean;
@@ -16,6 +17,7 @@ import org.springframework.web.socket.handler.PerConnectionWebSocketHandler;
 @Configuration
 @EnableWebSocket
 @ConditionalOnWebApplication
+@ConditionalOnClass(WebSocketConfigurer.class)
 @ConditionalOnProperty(name = "graphql.spqr.websocket.enabled", havingValue = "true", matchIfMissing = true)
 @ConditionalOnBean(GraphQLSchema.class)
 public class SpqrWebSocketAutoConfiguration implements WebSocketConfigurer {
