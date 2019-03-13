96b5588c22b54c6725827e3892c37941718ca7cf
Robert  Dinu <vjroby@gmail.com>
2019-03-13 20:03:01 +0100

    Refactored.

--- graphql-spqr-spring-boot-autoconfigure/src/test/java/io/leangen/graphql/spqr/spring/web/GraphQLControllerTest.java
+++ graphql-spqr-spring-boot-autoconfigure/src/test/java/io/leangen/graphql/spqr/spring/web/GraphQLControllerTest.java
@@ -138,11 +138,10 @@ public class GraphQLControllerTest {
                 .andExpect(content().string(containsString("Hello world")));
     }
 
-    // TODO: move to another class plus more tests
     @Test
-    public void pagereq() throws Exception {
+    public void defaultControllerTest_POST_spring_page() throws Exception {
 
-        String withpage ="{\n" +
+        String withPage ="{\n" +
                 "    springPageComponent_users(first:3,after:\"2id\") {\n" +
                 "        pageInfo {\n" +
                 "            startCursor\n" +
@@ -170,9 +169,10 @@ public class GraphQLControllerTest {
                 "        }\n" +
                 "    }\n" +
                 "}\n";
+
         mockMvc.perform(
                 get("/"+apiContext)
-                        .param("query",withpage))
+                        .param("query",withPage))
                 .andExpect(status().isOk())
                 .andExpect(content().string(equalToIgnoringWhiteSpace("{" +
                         "\"data\":{\"springPageComponent_users\":{\"pageInfo\":{\"startCursor\":\"1\",\"endCursor\":\"3\",\"hasNextPage\":true}," +
